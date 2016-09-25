7981d98bedd2583f91288052ea291293e52eb022
popac <web.guru.serbia@gmail.com>
2016-09-26 00:36:02 +0200

    Add Guided Local Search

--- lib/Algorithmically.rb
+++ lib/Algorithmically.rb
@@ -1,6 +1,7 @@
 require 'Algorithmically/version'
 require 'Algorithmically/Stochastic/random_search'
 require 'Algorithmically/Stochastic/hill_climbing'
+require 'Algorithmically/Stochastic/guided_local_search'
 
 
 module Algorithmically
